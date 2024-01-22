70475c01f3865498ffa490f1431ba33e35c35382
Siem Broeder <siembroeder@gmail.com>
2024-01-22 17:33:45 +0100

    Now the data from all the files is written to one file in a correct .json format

--- filter_instruments.py
+++ filter_instruments.py
@@ -5,6 +5,10 @@ import csv
 filepaths = ['./A_people.json', './B_people.json', './C_people.json', './D_people.json', './E_people.json', './F_people.json', './G_people.json', './H_people.json', './I_people.json', './J_people.json', './K_people.json', './L_people.json', './M_people.json', './N_people.json', './O_people.json', './P_people.json', './Q_people.json', './R_people.json', './S_people.json', './T_people.json', './U_people.json', './V_people.json', './W_people.json', './X_people.json', './Y_people.json', './Z_people.json']                                             
 
 
+# Store the entries from each file in a list
+total_output = []
+
+
 # Loop over all files
 for file in filepaths:
 
@@ -12,20 +16,23 @@ for file in filepaths:
     with open(file, encoding='utf-8') as file:
         data = json.load(file)
 
-    # Store the entries that are extracting
-    output = []
+    # Store the entries that are being extracted
+    output_loop = []
 
     # Loop over all the entries in the current file
     for entry in data:
 
         # If both the instrument label and birthdate are present in the entry, extract the entry
         if 'ontology/instrument_label' in entry and 'ontology/birthDate' in entry:
-            output.append(entry)
+            output_loop.append(entry)
 
-    # Write the list that contains the filtered entries to a json file
-    with open('bigdata.json', 'w', encoding='utf-8') as file:
-        json.dump(output, file)
-    
+    total_output.append(output_loop)
+
+
+# Write the list that contains the filtered entries to a json file
+with open('bigdata.json', 'w', encoding='utf-8') as file:
+    json.dump(total_output, file)
+
 
 
 
