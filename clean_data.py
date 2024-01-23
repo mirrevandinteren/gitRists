f1976a934b677a542e3388d1e6b563ab34f8f2a5
Blanka <csgb@zs2.hu>
2024-01-23 10:32:45 +0100

    Cleaning step 1: removed upper cases from CSV data

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from csv import DictReader
 
 with open("bigdata.json", encoding='utf-8') as file:
     clean_data = json.load(file)
@@ -8,9 +9,22 @@ with open("bigdata.json", encoding='utf-8') as file:
 # Extract the fields for the CSV file
 fields = ['ontology/instrument_label', 'ontology/birthDate']
 
-# Open a new CSV
+# Create a new CSV
 with open('clean_data.csv', 'w', encoding= 'utf-8') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(fields)
     for row in clean_data:
         writer.writerow([row[field] for field in fields])
+
+# Clean the data
+
+# Open the newly created CSV file     
+with open('clean_data.csv', encoding='utf-8') as file:
+    reader=DictReader(file)
+    csv_data = list(reader)
+
+# Remove upper cases for intruments
+for entry in csv_data:
+    entry['ontology/instrument_label']=entry['ontology/instrument_label'].lower()
+
+print(csv_data)
