50f0ee4082d1bc87da07d22250ea609d4b016ddd
Mirre <mirrevandinteren@gmail.com>
2024-01-24 14:30:18 +0100

    distribution pianists plot

--- pianist_distribution_genre.R
+++ pianist_distribution_genre.R
@@ -1,8 +1,24 @@
 library(tidyverse)
 
-data <- read_delim('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/piano_instrument.csv', delim = ',')
+piano_genre <- read_csv('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/piano_genre_counter.csv')
 
-piano_genre <- data |> select(decade, instrument_label, pop_music, classical_music) |>
-  mutate(musical = ifelse(instrument_label == "piano", "pianist", "musician")) |>
-  select(decade, musical, pop_music, classical_music) |>
+piano_genre <- piano_genre |>
+  mutate(ratio_pop = pop_pianists / total_pianists) |>
+  mutate(ratio_classical = classical_pianists / total_pianists) |>
+  mutate(ratio_jazz = jazz_pianists / total_pianists) |>
+  mutate(ratio_other = (total_pianists - jazz_pianists - classical_pianists - pop_pianists) / total_pianists)
+
+ggplot(data = piano_genre) +
+  xlim(1850,2020) +
+  scale_y_continuous(labels = scales::label_percent()) +
+  geom_line(aes(x=decade, y=ratio_pop), color=rgb(255,105,180, maxColorValue = 255)) +
+  geom_line(aes(x=decade, y=ratio_classical), color=rgb(65,105,225, maxColorValue = 255)) +
+  geom_line(aes(x=decade, y=ratio_jazz), color=rgb(246,169,15, maxColorValue = 255)) +
+  geom_line(aes(x=decade, y=ratio_other)) +
+  xlab('Time') +
+  ylab('Pianists distribution')
+ggsave('Pianist_distribution_genre.pdf')
+  
+
+
   
