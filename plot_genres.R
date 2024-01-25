e404d523c11ae0c85cfc29d9b00452609ed0e74e
Mirre <mirrevandinteren@gmail.com>
2024-01-25 10:55:35 +0100

    message

--- plot_genres.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-data <- read_delim('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/piano_instrument.csv', delim = ',')
-
-piano_genre <- data |> select(decade, instrument_label, pop_music, classical_music) |>
-  mutate(musical = ifelse(instrument_label == "piano", "pianist", "musician")) |>
-  select(decade, musical, pop_music, classical_music) |>
-  
--- plot_instrument_per_decade.R
+++ plot_instrument_per_decade.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
-data <- read_delim('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/clean_data.csv', delim = ';')
+data <- read_csv('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/genre_counter.csv')
 
-new_data <- data |> select(decade, instrument_label) |>
-  filter(instrument_label == 'Piano') |>
-  group_by(decade)
+new_data <- data |>
+  select(total_pianists, five_year_bracket) |>
+  group_by(five_year_bracket)
 
 ggplot(data = new_data) +
-  aes(x = decade) +
+  aes(x = five_year_bracket, y = total_pianists) +
   xlim(1800,2020) +
   xlab('Time') +
   ylab('Number of pianists') +
   ggtitle('Pianists over the decades', subtitle = 'from 1800 onwards') +
-  geom_bar()
+  geom_col()
 ggsave('Piano_graph.pdf')
