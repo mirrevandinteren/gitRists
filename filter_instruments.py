2bb4586d1a0dfd57c82af5a6f7512f4e4a3fd1e4
Siem Broeder <siembroeder@gmail.com>
2024-01-25 10:45:29 +0100

    Added a metadata section at the beginning of all three files. This section includes authors, date, description of the function of the file.

--- filter_instruments.py
+++ filter_instruments.py
@@ -1,5 +1,23 @@
+
+'''
+Authors: Mirre van Dinteren, Siem Broeder, Blanka Cseres-Gergely, Kate Grossenbacher
+Date: 26 January 2024
+
+
+Description:
+
+This script parses through the People of Wikipedia dataset (2015), and selects the musicians from the list of people.
+The musicians are selected based on the 'ontology/instrument_label' to be present, for this research it's essential that a birthdate
+is also present. 
+The people that pass through this filter are then written to a .json file to be used in analysis later.
+
+'''
+
+
+
+
+
 import json
-import csv
 
 # Specify all the files from which we extract data, assume that we're in the right working directory
 filepaths = ['./A_people.json', './B_people.json', './C_people.json', './D_people.json', './E_people.json', './F_people.json', './G_people.json', './H_people.json', './I_people.json', './J_people.json', './K_people.json', './L_people.json', './M_people.json', './N_people.json', './O_people.json', './P_people.json', './Q_people.json', './R_people.json', './S_people.json', './T_people.json', './U_people.json', './V_people.json', './W_people.json', './X_people.json', './Y_people.json', './Z_people.json']                                             
