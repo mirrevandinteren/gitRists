9b8f87f5f39dee57a270bb07c0039537f9ad078f
Mirre <mirrevandinteren@gmail.com>
2024-01-26 17:13:31 +0100

    more plots

--- R_visualisation_scripts/plot_instrument_per_decade.R
+++ R_visualisation_scripts/plot_instrument_per_decade.R
@@ -3,14 +3,21 @@ library(tidyverse)
 data <- read_csv('C:/Users/Mirre/OneDrive/Documenten/Personal/School/Labcourses/Datascience/group_project/People/genre_counter.csv')
 
 new_data <- data |>
-  select(total_pianists, five_year_bracket) |>
+  select(total_pianists, total_guitarists, five_year_bracket) |>
   group_by(five_year_bracket)
 
 ggplot(data = new_data) +
   aes(x = five_year_bracket, y = total_pianists) +
   xlim(1800,2020) +
-  xlab('Time') +
+  xlab('Birthyear of musician') +
   ylab('Number of pianists') +
-  ggtitle('Pianists over the decades', subtitle = 'from 1800 onwards') +
   geom_col()
 ggsave('Piano_graph.pdf')
+
+ggplot(data = new_data) +
+  aes(x = five_year_bracket, y = total_guitarists) +
+  xlim(1800,2020) +
+  xlab('Birthyear of musician') +
+  ylab('Number of guitarists') +
+  geom_col()
+ggsave('Guitar_graph.pdf')
